58ed01e87e46306fa0ef55fecb9d127377405190
Everton Lima <evertonl@gmail.com>
2022-10-31 21:38:52 -0300

    Mudança no upload do arquivo
    Signed-off-by: Everton Lima <evertonl@gmail.com>

--- backend/app/Http/Controllers/ImageController.php
+++ backend/app/Http/Controllers/ImageController.php
@@ -12,6 +12,6 @@ class ImageController extends Controller
     {
         $file_name = Str::random() . '.' . $request->file('image')->extension();
 
-        return ['url' => config('filesystems.disks.public.url') . '/' . $request->file('image')->storeAs('images/products', $file_name, 'public')];
+        return ['url' => config('app.url') . '/' . config('filesystems.disks.public.url') . '/' . $request->file('image')->storeAs('images/products', $file_name, 'public')];
     }
 }
