d541b5d1656562746c0decdc7233fb7e2749ee94
Everton Lima <evertonl@gmail.com>
2022-12-29 21:20:11 -0300

    repost

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    watchFiles: {
+      options: {
+        ignored: /node_modules/,
+        usePolling: true,
+        awaitWriteFinish: {
+          stabilityThreshold: 2000,
+          pollInterval: 100
+        }
+      }
+    },
+  }
 })
